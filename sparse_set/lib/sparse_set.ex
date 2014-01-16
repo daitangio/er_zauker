f47ae4e564e1024d136599a1534bb2288f562f1f
Giorgig <giovanni.giorgi@valueteam.com>
2014-01-16 18:40:44 +0100

    Not working implementation

--- sparse_set/lib/sparse_set.ex
+++ sparse_set/lib/sparse_set.ex
@@ -1,2 +1,52 @@
 defmodule SparseSet do
+	@moduledoc	"""
+For a complete explanation see
+http://research.swtch.com/sparse
+
+ Operation 	Bit Vector 	Sparse set
+ is-member 	O(1) 	      O(1)
+ add-member O(1) 	      O(1)
+ clear-set 	O(m) 	      O(1)
+  iterate 	O(m) 	      O(n)
+  """
+
+	defrecord SSet, dense: {}, sparse: {}, n: 0 
+
+	defprotocol Sparse do
+		def isMember?(set,integer)
+		def add(set,i)
+		# def clear(set)
+		# def iterate(set,f)
+
+	end
+
+	# Protocol implementation for SSet
+	defimpl Sparse, for: SSet do
+
+		
+
+		def isMember?(set,i) do
+			IO.puts "isMember? set=#{inspect(set)} i=#{i}"
+			if set.n==0 do
+				false
+			else
+			
+				sparse_i= elem set.sparse,i 
+				IO.puts "sparse_i: #{sparse_i}"
+
+		    #return sparse[i] < n && dense[sparse[i]] == i
+		    (elem(set.sparse , i)) < set.n and ((elem set.dense, elem(set.sparse,i)) == i)
+			end
+		end
+	 def add(set,i) do 
+		 IO.puts "Adding..."
+		  # Ensure enough size...
+		  #dense: {}, sparse: {}, n: 0 
+		  #SSet.new(dense: s.dense
+		      #dense[n] = i
+					#sparse[i] = n
+					#n++
+	    SSet.new()
+	 end
+	end
 end
--- sparse_set/test/sparse_set_test.exs
+++ sparse_set/test/sparse_set_test.exs
@@ -1,7 +1,28 @@
 defmodule SparseSetTest do
   use ExUnit.Case
 
+	# Test cas eimport everything
+	import SparseSet, only: [ :all ]
+
   test "the truth" do
     assert(true)
   end
+
+	test "simple new" do
+		s= SparseSet.SSet.new
+		assert (s != nil)
+	end
+
+	test "empty set is empty" do
+		s =SparseSet.SSet.new()
+		assert SparseSet.Sparse.isMember?(s, 0) ==false
+	end
+
+	test "simple add" do
+		s=SparseSet.SSet.new
+		IO.puts inspect s
+		s2=SparseSet.Sparse.add( s, 42)		
+		#s2=s.add(42)
+		assert SparseSet.Sparse.isMember?(s2,42) == true
+	end
 end
