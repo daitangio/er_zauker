de9fd763904161274c44cd2dd61553fc722c30e5
jj <giovanni.giorgi@nttdata.com>
2014-08-25 16:49:38 +0200

    Coros: standalone cowboy final nitrogen web search interface

---
+++ web-interface/coros/site/src/nitrogen_app.erl
@@ -0,0 +1,17 @@
+-module(nitrogen_app).
+-behaviour(application).
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    io:format("~n-------------------------------"),
+    io:format("~n-- COROS Starting Zauker ----~n"),
+    %% er_zauker_app:startIndexer(),
+    %% io:format("~n-- COROS Zauker Indexer OK ----~n"),    
+    nitrogen_sup:start_link().
+
+stop(_State) ->
+    ok.
